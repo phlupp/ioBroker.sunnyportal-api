0f4946e1cb88b198003530dfab39fdfd54178974
Phlupp <philipp.harsch@icloud.com>
2021-03-22 23:10:31 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"sunnyportal-api adapter settings": {
+		"en": "Adapter settings for sunnyportal-api",
+		"de": "Adaptereinstellungen für sunnyportal-api",
+		"ru": "Настройки адаптера для sunnyportal-api",
+		"pt": "Configurações do adaptador para sunnyportal-api",
+		"nl": "Adapterinstellingen voor sunnyportal-api",
+		"fr": "Paramètres d'adaptateur pour sunnyportal-api",
+		"it": "Impostazioni dell'adattatore per sunnyportal-api",
+		"es": "Ajustes del adaptador para sunnyportal-api",
+		"pl": "Ustawienia adaptera dla sunnyportal-api",
+		"zh-cn": "sunnyportal-api的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
